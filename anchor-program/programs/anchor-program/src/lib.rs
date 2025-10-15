a3e2edefeab24d3fd6014b05d7f1973c580389cd
Akshit Vig <akshitvig48@gmail.com>
2025-10-15 22:20:04 +0530

    added better txns logic

--- anchor-program/programs/anchor-program/src/lib.rs
+++ anchor-program/programs/anchor-program/src/lib.rs
@@ -1,9 +1,11 @@
 use anchor_lang::prelude::*;
 
-declare_id!("7i6pU54rKPsfuvdzmEVe9W8fpLA1vtZUbmZfbXamrrxn");
+declare_id!("CETiLPyUefJ5FT7TkARKM7W2NhGxDHmze8CiSqptNFFZ");
 
 #[program]
 pub mod anchor_program {
+
+    use anchor_lang::prelude::program::invoke_signed;
 
     use super::*;
 
@@ -19,16 +21,22 @@ pub mod anchor_program {
         session.duration = duration;
         session.status = 0;
 
-        **ctx
-            .accounts
-            .user
-            .to_account_info()
-            .try_borrow_mut_lamports()? -= session.stake_amount;
-        **ctx
-            .accounts
-            .vault
-            .to_account_info()
-            .try_borrow_mut_lamports()? += session.stake_amount;
+        let session_key = session.key();
+        let vault_seeds = &[b"vault", session_key.as_ref(), &[ctx.accounts.vault.bump]];
+
+        invoke_signed(
+            &system_instruction::transfer(
+                &ctx.accounts.user.key(),
+                &ctx.accounts.vault.key(),
+                stake_amount,
+            ),
+            &[
+                ctx.accounts.user.to_account_info(),
+                ctx.accounts.vault.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[vault_seeds],
+        )?;
 
         ctx.accounts.vault.bump = ctx.bumps.vault;
 
@@ -48,21 +56,36 @@ pub mod anchor_program {
         let user_share = session.stake_amount * 99 / 100;
         let pool_share = session.stake_amount - user_share;
 
-        **ctx
-            .accounts
-            .vault
-            .to_account_info()
-            .try_borrow_mut_lamports()? -= session.stake_amount;
-        **ctx
-            .accounts
-            .user
-            .to_account_info()
-            .try_borrow_mut_lamports()? += user_share;
-        **ctx
-            .accounts
-            .focus_pool
-            .to_account_info()
-            .try_borrow_mut_lamports()? += pool_share;
+        let session_key = session.key();
+        let vault_seeds = &[b"vault", session_key.as_ref(), &[ctx.accounts.vault.bump]];
+
+        invoke_signed(
+            &system_instruction::transfer(
+                &ctx.accounts.vault.key(),
+                &ctx.accounts.user.key(),
+                user_share,
+            ),
+            &[
+                ctx.accounts.vault.to_account_info(),
+                ctx.accounts.user.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[vault_seeds],
+        )?;
+
+        invoke_signed(
+            &system_instruction::transfer(
+                &ctx.accounts.vault.key(),
+                &ctx.accounts.focus_pool.key(),
+                pool_share,
+            ),
+            &[
+                ctx.accounts.vault.to_account_info(),
+                ctx.accounts.focus_pool.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[vault_seeds],
+        )?;
 
         session.status = 1;
         msg!("Session completed for {:?}", ctx.accounts.user.key());
@@ -78,16 +101,22 @@ pub mod anchor_program {
             ErrorCode::NotExpired
         );
 
-        **ctx
-            .accounts
-            .vault
-            .to_account_info()
-            .try_borrow_mut_lamports()? -= session.stake_amount;
-        **ctx
-            .accounts
-            .failure_pool
-            .to_account_info()
-            .try_borrow_mut_lamports()? += session.stake_amount;
+        let session_key = session.key();
+        let vault_seeds = &[b"vault", session_key.as_ref(), &[ctx.accounts.vault.bump]];
+
+        invoke_signed(
+            &system_instruction::transfer(
+                &ctx.accounts.vault.key(),
+                &ctx.accounts.failure_pool.key(),
+                session.stake_amount,
+            ),
+            &[
+                ctx.accounts.vault.to_account_info(),
+                ctx.accounts.failure_pool.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[vault_seeds],
+        )?;
 
         session.status = 2;
         msg!("Session expired for {:?}", session.user);
@@ -142,6 +171,7 @@ pub struct CompleteSession<'info> {
     ///CHECK: This is the focus_pool account that receives 1% of completed session stakes
     #[account(mut)]
     pub focus_pool: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -155,6 +185,7 @@ pub struct ExpireSession<'info> {
     ///CHECK: This is the failure_pool account that receives stakes from failed sessions
     #[account(mut)]
     pub failure_pool: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[error_code]
